add6585520e667a1757d9f64a450df1345bf1f5e
Marija <nesto@gmail.com>
2024-08-21 23:04:28 +0200

    Script for filtering images

--- components/image_filtering.py
+++ components/image_filtering.py
@@ -1,20 +1,26 @@
 import cv2
 import numpy as np
-from skimage import morphology
-from skimage.restoration import estimate_sigma, denoise_nl_means
-from skimage import img_as_ubyte
-from scipy.ndimage import gaussian_filter
 
-def filter_image(img, filter_method):
-    if filter_method == 'Original':
-        return img
-    if filter_method == 'Denoise':
-        filtered_image = denoise_nl_means(img)
-        return filtered_image
+def filter_image(images, filter_method = 'non_local_means', **kwargs):
+    denoised_images = []
 
-def denoise_nl_means(img):
-    sigma_est = np.mean(estimate_sigma(img, channel_axis=None))
-    denoise = denoise_nl_means(img, h=1.15 * sigma_est, sigma = sigma_est, fast_mode=True, patch_size=5, patch_distance=3)
-    denoise_ubyte = img_as_ubyte(denoise)
-    return denoise_ubyte
+    for img in images:
+        if filter_method == 'non_local_means':
+            denoised_img = cv2.fastNlMeansDenoising(img, None, h=10, templateWindowSize=7, searchWindowSize=21)
+
+        elif filter_method == 'gaussian':
+            kernel_size = kwargs.get('kernel_size', (5, 5))
+            std_dev_xy = kwargs.get('std_dev_xy', 0)
+            denoised_img = cv2.GaussianBlur(img, kernel_size, std_dev_xy)
+
+        elif filter_method == 'median':
+            kernel_size = kwargs.get('kernel_size', 5)
+            denoised_img = cv2.medianBlur(img, kernel_size)
+
+        else:
+            raise ValueError("Nepoznati metod za filtiranje")
+        
+        denoised_images.append(denoised_img)
+    
+    return denoised_images
 
